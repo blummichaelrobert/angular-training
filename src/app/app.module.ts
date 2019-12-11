7e9aca3e8afd1bd85eb525b39a74cd56f47a7dd3
Michael Blum <Michael.Blum@prosearch.us>
2019-12-11 10:24:34 -0800

    create folder structure form project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { CoreComponent } from './core/core.component';
+import { CustomerComponent } from './customer/customer.component';
+import { CustomersComponent } from './customers/customers.component';
+import { LoginComponent } from './login/login.component';
+import { OrdersComponent } from './orders/orders.component';
+import { SharedComponent } from './shared/shared.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    CoreComponent,
+    CustomerComponent,
+    CustomersComponent,
+    LoginComponent,
+    OrdersComponent,
+    SharedComponent
   ],
   imports: [
     BrowserModule,
