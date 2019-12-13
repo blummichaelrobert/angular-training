7477967c7bfa8d4d3a6d6521f9f703b908f67419
Michael Blum <Michael.Blum@prosearch.us>
2019-12-13 14:29:35 -0800

    add modal component code

--- src/app/core/modal/modal.component.ts
+++ src/app/core/modal/modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IModalContent, ModalService } from './modal.service';
 
 @Component({
   selector: 'cm-modal',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalComponent implements OnInit {
 
-  constructor() { }
+  modalVisible = false;
+  modalVisibleAnimate = false;
+  modalContent: IModalContent = {};
+  cancel: () => void;
+  ok: () => void;
+  defaultModalContent: IModalContent = {
+    header: 'Please Confirm',
+    body: 'Are you sure you want to continue?',
+    cancelButtonText: 'Cancel',
+    OKButtonText: 'OK',
+    cancelButtonVisible: true
+  };
+
+  constructor(private modalService: ModalService) { 
+    modalService.show = this.show.bind(this);
+    modalService.hide = this.hide.bind(this);
+  }
 
   ngOnInit() {
+
+  }
+
+  show(modalContent: IModalContent) {
+    this.modalContent = Object.assign(this.defaultModalContent, modalContent);
+    this.modalVisible = true;
+    setTimeout(() => this.modalVisibleAnimate = true);
+
+    const promise = new Promise<boolean>((resolve, reject) => {
+      this.cancel = () => {
+        this.hide();
+        resolve(false);
+      };
+    });
+    return promise;
+  }
+
+  hide() {
+    this.modalVisibleAnimate = false;
+    setTimeout(() => this.modalVisible = false, 300);
   }
 
 }
