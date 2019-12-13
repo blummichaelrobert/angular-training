b0266c2ef95629ccefb8fab17fb8b8a4fe4f1276
Michael Blum <Michael.Blum@prosearch.us>
2019-12-13 14:51:59 -0800

    add code for overlay component

--- src/app/core/overlay/overlay.component.ts
+++ src/app/core/overlay/overlay.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { EventBusService, Events } from '../services/event-bus.service';
 
 @Component({
   selector: 'cm-overlay',
   templateUrl: './overlay.component.html',
   styleUrls: ['./overlay.component.css']
 })
-export class OverlayComponent implements OnInit {
+export class OverlayComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  httpRequestSub: Subscription;
+  httpResponseSub: Subscription;
+  enabled = false;
+  queue = [];
+  timerId: number = null;
+  timerHideId: number = null;
+
+  @Input() delay = 500;
+
+  constructor(private eventBus: EventBusService) { }
 
   ngOnInit() {
+    // Handle Request
+    this.httpRequestSub = this.eventBus.on(Events.httpRequest, (() => {
+      this.queue.push({});
+      if (this.queue.length === 1) {
+        // Only show if we have an item in the queue after the delay time
+        setTimeout(() => { if (this.queue.length) { this.enabled = true; } }, this.delay);
+      }
+    }));
+
+    // Handle Response
+    this.httpResponseSub = this.eventBus.on(Events.httpResponse, (() => {
+      this.queue.pop();
+      if (this.queue.length === 0) {
+        setTimeout(() => { if (this.queue.length === 0) { this.enabled = false; } }, this.delay);
+      }
+    }));
+  }
+
+  ngOnDestroy(): void {
+    this.httpRequestSub.unsubscribe();
+    this.httpResponseSub.unsubscribe();
   }
 
 }
--- src/app/core/services/event-bus.service.ts
+++ src/app/core/services/event-bus.service.ts
@@ -3,7 +3,24 @@ import { Subject, Subscription, Observable } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 
 @Injectable()
-export class EventBusService { }
+export class EventBusService {
+
+    subject = new Subject<any>();
+
+    constructor() { }
+
+    on(event: Events, action: any): Subscription {
+        return this.subject
+        .pipe(
+            filter((e: EmitEvent) => { return e.name === event; }),
+            map((e: EmitEvent) => { return e.value }),
+        ).subscribe(action);
+    }
+
+    emit(event: EmitEvent) {
+        this.subject.next(event);
+    }
+}
 
 export class EmitEvent {
     constructor(public name: any, public value?: any) { }
