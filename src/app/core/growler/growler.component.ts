927e788fcf21ade559711b71eb6fa049ba2d8963
Michael Blum <Michael.Blum@prosearch.us>
2019-12-13 11:01:47 -0800

    implement core/growler

--- src/app/core/growler/growler.component.ts
+++ src/app/core/growler/growler.component.ts
@@ -19,15 +19,63 @@ export class GrowlerComponent implements OnInit {
   constructor(private growlerService: GrowlerService,
               private logger: LoggerService
              ) {
-    // growlerService.growl = this.growl.bind(this);
+     growlerService.growl = this.growl.bind(this);
   }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  /**
+  * Displays a growl message.
+  *
+  * @param {string} message - The message to display.
+  * @param {GrowlMessageType} growlType - The type of message to display (a GrowlMessageType enumeration)
+  * @return {number} id - Returns the ID for the generated growl
+  */
+
+  growl(message: string, growlType: GrowlerMessageType): number {
+    this.growlCount++;
+    const bootstrapAlertType = GrowlerMessageType[growlType].toLocaleLowerCase();
+    const messageType = `alert-${ bootstrapAlertType }`;
+
+    const growl = new Growl(this.growlCount, message, messageType, this.timeout, this);
+    this.growls.push(growl);
+    return growl.id;
   }
 
+  removeGrowl(id: number) {
+    this.growls.forEach((growl: Growl, index: number) => {
+      if (growl.id === id) {
+        this.growls.splice(index, 1);
+        this.growlCount--;
+        this.logger.log('removed' + id);
+      }
+    });
+  }
 }
 
 class Growl {
   enabled: boolean;
   timeoutId: number;
+
+  constructor(public id: number,
+              public message: string,
+              public messageType: string,
+              private timeout: number,
+              private growlerContainer: GrowlerComponent) {
+
+    this.show();
+  }
+
+  show() {
+    window.setTimeout(() => {
+      this.hide();
+    }, this.timeout);
+  }
+
+  hide() {
+    this.enabled = false;
+    window.setTimeout(() => {
+      this.growlerContainer.removeGrowl(this.id);
+    }, this.timeout);
+  }
 }
