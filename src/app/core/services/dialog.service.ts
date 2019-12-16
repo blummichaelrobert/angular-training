74ae958f4d7b62b81e362ad6fb5e36f8330d1dd8
Michael Blum <Michael.Blum@prosearch.us>
2019-12-16 10:53:50 -0800

    add core services

--- src/app/core/services/dialog.service.ts
+++ src/app/core/services/dialog.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@angular/core';
 
 @Injectable()
-export class DialogService { }
+export class DialogService { 
+
+    promise: Promise<boolean>;
+    message = 'Is it okay?';
+
+    confirm(message?: string) {
+        if (message) {this.message = message; }
+        this.promise = new Promise<boolean>(this.resolver);
+        return this.promise;
+    }
+
+    resolver(resolve: any) {
+        return resolve(window.confirm('Is it okay?'));
+    }
+}
--- src/app/core/services/trackby.service.ts
+++ src/app/core/services/trackby.service.ts
@@ -3,4 +3,13 @@ import { Injectable } from '@angular/core';
 import { ICustomer, IOrder } from '../../shared/interfaces';
 
 @Injectable()
-export class TrackByService { }
+export class TrackByService { 
+
+    customer(index: number, customer: ICustomer) {
+        return customer.id;
+    }
+
+    order(index: number, order: IOrder) {
+        return index;
+    }
+}
