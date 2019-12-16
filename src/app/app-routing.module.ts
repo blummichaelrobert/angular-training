2af94d87a6a17a692eb97d39eb8af6ec2fae98b0
Michael Blum <Michael.Blum@prosearch.us>
2019-12-16 15:20:16 -0800

    add app-routing, fix name fo preload modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes, PreloadAllModules, NoPreloading } from '@angular/router';
 
-const routes: Routes = [];
+import { PreloadModulesStrategy } from './core/strategies/preload-modules.strategy';
+
+const app_routes: Routes = [
+  { path: 'customers/:id', data: { preload: true }, loadChildren: 'app/customer/customer.module#CustomerModule' },
+  { path: 'customers', loadChildren: 'app/customers/customers.module#CustomersModule' },
+  { path: 'orders', data: { preload: true }, loadChildren: 'app/orders/orders.module#OrdersModule' },
+  { path: 'about', loadChildren: 'app/about/about.module#AboutModule' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(app_routes, { preloadingStrategy: PreloadModulesStrategy }) ],
+  exports: [ RouterModule ],
+  providers: [PreloadModulesStrategy]
 })
 export class AppRoutingModule { }
--- src/app/core/strategies/preload-modules.strategy.ts
+++ src/app/core/strategies/preload-modules.strategy.ts
@@ -2,10 +2,9 @@ import { Injectable } from '@angular/core';
 import { PreloadingStrategy, Route } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { LoggerService } from '../services/logger.service';
-import { load } from '@angular/core/src/render3';
 
 @Injectable()
-export class ProloadModulesStrategy implements PreloadingStrategy {
+export class PreloadModulesStrategy implements PreloadingStrategy {
 
     constructor(private logger: LoggerService) { }
 
