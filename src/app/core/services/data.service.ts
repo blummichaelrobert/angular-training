06b65cfd4f35b431228ff6993814baf8904460af
Michael Blum <Michael.Blum@prosearch.us>
2019-12-16 09:05:21 -0800

    add data service and preload modules strategy

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -1,10 +1,81 @@
-import { Injectable, Inject } from '@angular/core';
+import { Injectable, Inject, inject } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 
 import { ICustomer, IOrder, IState, IPagedResults, IApiResponse } from '../../shared/interfaces';
 
 @Injectable()
-export class DataService { }
+export class DataService { 
+
+    // Can use /api/customers and /api/orders below when running locally
+    // Full domain/port is included for Docker example or if it were to run in the cloud
+    port = (this.window.location.port) ? ':' + this.window.location.port : '';
+    baseUrl = `${this.window.location.protocol}//${this.window.location.hostname}${this.port}`;
+    customerBaseUrl = this.baseUrl + 'api/customers';
+    orderBaseUrl = this.baseUrl + 'api/orders';
+    orders: IOrder[];
+    states: IState[];
+
+    constructor(private http: HttpClient, @Inject('Window') private window: Window) { }
+
+    getCustomersPage(page: number, pageSize: number): Observable<IPagedResults<ICustomer[]>> {
+        return this.http.get<ICustomer[]>(`${this.customerBaseUrl}/page/${page}/${pageSize}`, { observe: 'response' })
+        .pipe(map(res => {
+            const totalRecords = +res.headers.get('X-InlineCount');
+            const customers = res.body as ICustomer[];
+            this.calculateCustomersOrderTotal(customers);
+            return { results: customers, totalRecords: totalRecords };
+        }),
+        catchError(this.handleError));
+    }
+
+    getCustomers(): Observable<ICustomer[]> {
+        return this.http.get<ICustomer[]>(this.customerBaseUrl)
+        .pipe(map(customers => {
+            this.calculateCustomersOrderTotal(customers);
+            return customers;
+        }),
+        catchError(this.handleError));
+    }
+
+    insertCustomer(customer: ICustomer): Observable<ICustomer> {
+        return this.http.post<ICustomer>(this.customerBaseUrl, customer).pipe(catchError(this.handleError));
+    }
+
+    updateCustomer(customer: ICustomer): Observable<boolean> {
+        return this.http.put<IApiResponse>(this.customerBaseUrl + '/' + customer.id, customer)
+        .pipe(map(res => res.status), catchError(this.handleError));
+    }
+
+    deleteCustomer(id: number): Observable<boolean> {
+        return this.http.delete<IApiResponse>(this.customerBaseUrl + '/' + id)
+        .pipe(map(res => res.status), catchError(this.handleError));
+    }
+
+    getStates(): Observable<IState[]> {
+        return this.http.get<IState[]>('/api/states').pipe(catchError(this.handleError));
+    }
+
+    calculateCustomersOrderTotal(customers: ICustomer[]) {
+        for (const customer of customers) {
+            if (customer && customer.orders) {
+                let total = 0;
+                for (const order of customer.orders) {
+                    total += order.itemCost;
+                }
+                customer.orderTotal = total;
+            }
+        }
+    }
+
+    private handleError(error: HttpErrorResponse) {
+        console.error('server error:', error);
+        if (error.error instanceof Error) {
+            const errMessage = error.error.message;
+            return throwError(errMessage);
+        }
+        return throwError(error || 'Node.js server error');
+    }
+}
--- src/app/core/strategies/preload-modules.strategy.ts
+++ src/app/core/strategies/preload-modules.strategy.ts
@@ -2,10 +2,20 @@ import { Injectable } from '@angular/core';
 import { PreloadingStrategy, Route } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { LoggerService } from '../services/logger.service';
+import { load } from '@angular/core/src/render3';
 
 @Injectable()
 export class ProloadModulesStrategy implements PreloadingStrategy {
-    preload(route: Route, fn: () => Observable<any>): Observable<any> {
-        throw new Error("Method not implemented.");
+
+    constructor(private logger: LoggerService) { }
+
+    preload(route: Route, load: () => Observable<any>): Observable<any> {
+        const preload = 'preload';
+        if (route.data && route.data[preload]) {
+            this.logger.log('Preloaded: ' + route.path);
+            return load();
+        } else {
+            return of (null);
+        }
     }
 }
