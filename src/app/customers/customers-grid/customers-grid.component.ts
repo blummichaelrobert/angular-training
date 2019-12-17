4e9e66c5b6eb818641cb85bb35979db360c01570
Michael Blum <Michael.Blum@prosearch.us>
2019-12-17 13:58:46 -0800

    implement customers-grid

--- src/app/customers/customers-grid/customers-grid.component.ts
+++ src/app/customers/customers-grid/customers-grid.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+import { SorterService } from '../../core/services/sorter.service';
+import { TrackByService } from '../../core/services/trackby.service';
+import { ICustomer } from '../../shared/interfaces';
+
 
 @Component({
   selector: 'cm-customers-grid',
   templateUrl: './customers-grid.component.html',
-  styleUrls: ['./customers-grid.component.css']
+  styleUrls: ['./customers-grid.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CustomersGridComponent implements OnInit {
 
-  constructor() { }
+  @Input() customers: ICustomer[] = [];
+
+  constructor(private sorterService: SorterService, public trackByService: TrackByService) { }
 
   ngOnInit() {
+  }
+
+  sort(prop: string) {
+    this.sorterService.sort(this.customers, prop);
   }
 
 }
