318bfb8c7315065b21e09c59d181e72c2c250236
Michael Blum <Michael.Blum@prosearch.us>
2019-12-18 09:30:57 -0800

    implement filter textbox component

--- src/app/shared/filter-textbox/filter-textbox.component.ts
+++ src/app/shared/filter-textbox/filter-textbox.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'cm-filter-textbox',
   templateUrl: './filter-textbox.component.html',
   styleUrls: ['./filter-textbox.component.css']
 })
-export class FilterTextboxComponent implements OnInit {
+export class FilterTextboxComponent {
+
+  model: { filter: string } = { filter: null };
+
+  @Output()
+  changed: EventEmitter<string> = new EventEmitter<string>();
 
   constructor() { }
 
-  ngOnInit() {
+  filterChanged(event: any) {
+    event.preventDefault();
+    this.changed.emit(this.model.filter);
   }
 
 }
